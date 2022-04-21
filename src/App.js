955706e6b1d1963a51a22c6914e7b5418ede273b
Valery555 <sokolchenkov@gmail.com>
2022-04-22 01:29:22 +0300

    _1_

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-function App() {
-  return (
-   <div>
-Hello react!!!
-   </div>
-  );
+import React, {useReducer} from 'react';
+
+const init = (initCount) => {
+    return {count1:initCount,count2:initCount}
+
+
 }
+console.log(init);
+const reducer = () => {
+
+}
+
+
+const App = () => {
+    const [state, dispatch] = useReducer(reducer,0,init);
+    return (
+        <div>
+            app
+        </div>
+    );
+};
+
 
 export default App;
