e8b91ac495de7dc86b8b7cb2ecdbe4de2f1b8a22
Valery555 <sokolchenkov@gmail.com>
2022-04-24 20:54:49 +0300

    _1_

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import React, {useReducer} from 'react';
 
 const init = (initCount) => {
-    return {count1:initCount,count2:initCount}
+    return {count1: initCount,count2: initCount} // это попадает в state
 
 
 }
-console.log(init);
-const reducer = () => {
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'inc':
+            return {state,count1: state.count1 +1 }
+        case 'dec':
+            return {state,count1: state.count1 -1 }
+        case 'reset':
+            return {state,count1: 0 }
+        case 'install':
+            return {state,count1: action.payload }
+    }
 
 }
-
 
 const App = () => {
     const [state, dispatch] = useReducer(reducer,0,init);
     return (
         <div>
-            app
+           <div>
+
+               {state.count1} <br/>
+               <button onClick={()=>dispatch({type:'inc'})}>inc</button>
+               <button onClick={()=>dispatch({type:'dec'})}>dec</button>
+               <button onClick={()=>dispatch({type:'reset'})}>reset</button>
+               <button onClick={()=>dispatch({type:'install', payload: 10})}>install_10</button>
+
+           </div>
         </div>
     );
 };
