7fb6f49d1ccaf607b4b6d13319a846ef12839ad7
Valery555 <sokolchenkov@gmail.com>
2022-04-25 20:24:45 +0300

    _1_

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import {useRef} from "react";
+import {ADD, DELETE, useCatReducer} from "./reducers";
+
+
 function App() {
-  return (
-   <div>
-Hello react!!!
-   </div>
-  );
+
+    const catRef = useRef();
+    const [state, dispatch] = useCatReducer();
+
+    const addCat = () => {
+        const newCat = catRef.current.value;
+        dispatch({type: ADD, payload: {cat: newCat}})
+    }
+
+    return (
+        <div>
+            <div>
+                <input type="text" ref={catRef} placeholder={'enter cat name'}/>
+                <button onClick={addCat}>Add</button>
+            </div>
+            <hr/>
+            <div>
+                {state.map(cat => <div key={cat.id}>{cat.name}
+                    <button onClick={() => dispatch({type: DELETE, payload: {id: cat.id}})}>Delete</button>
+                </div>)}
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/reducers/cat.reducer.js
@@ -0,0 +1,26 @@
+import {ADD, DELETE} from "./actions";
+import {useReducer} from "react";
+
+const reducer = (state, action) => {
+
+    switch (action.type) {
+        case ADD:
+            const cat = action.payload.cat;
+
+            const [lastCat] = state.slice(-1);
+            const id = lastCat ? lastCat.id + 1 : 0;
+
+            return [...state, {id, name: cat}];
+
+        case DELETE:
+            const {id:catId} = action.payload;
+            const index = state.findIndex(cat => cat.id === catId);
+            state.splice(index, 1)
+            return [...state]
+    }
+};
+
+
+const useCatReducer = () => useReducer(reducer, []);
+
+export {useCatReducer}
