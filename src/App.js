edbce5c222a07efb7e12d30724cbfa7dcf9c7cf8
Valery555 <sokolchenkov@gmail.com>
2022-05-02 19:11:23 +0300

    redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import Redux from "./redux";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+
 function App() {
-  return (
-   <div>
-Hello react!!!
-   </div>
-  );
+
+
+    function reducer(state = 'privet Valera!!', action) {
+        switch (action.type) {
+            case 'change':
+                return action.payload;
+
+            case 'back':
+                return action.payload;
+
+            default:
+                return state
+        }
+
+    }
+
+    let store = createStore(reducer);
+    return (
+        <div>
+            <Provider store={store}>
+
+                <Redux props={'hello => через пропсы'}/>
+
+            </Provider>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/redux.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
+
+const Redux = ({props}) => {
+    let state = useSelector((state) => state);
+    let dispatch = useDispatch();
+    return (
+        <div>
+            <div>{props}</div>
+            <div>{state}</div>
+            <button onClick={() => dispatch ({type: 'change', payload:'poka Valera => через redux'})}>Изменить</button>
+            <button onClick={() => dispatch ({type: 'back', payload:'privet Valera => через redux!!'})}>Вернуть назад</button>
+        </div>
+    );
+};
+
+export default Redux;
